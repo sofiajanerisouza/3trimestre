46444bae9cfd5b146e8ee1e0b4fbbd48a7484605
sofia <105672340+sofiajanerisouza@users.noreply.github.com>
2024-03-18 08:21:50 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,45 +1,47 @@
-// Pom
-function tocaSomPom (seletorAudio)
-   const elemento = document.querySelector(seletorAudio);
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-contuedo");
 
-   if (elemento === null) {
-       //alert('Elemento nao encontrado');
-       console.log('Elemento nao encotrado ');
-   }
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick = function(){
 
-   if (elemento != null ) 
-  
-   HTMLFormControlsCollection.
+        for(let j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+        }   textos[j].classList.remove("ativo");
 
-   //elemento.play(); {
-     }
-}  
-
-const  listaDeTeclas = document.querySelectorAll('.tecla');
-
-
-//para
-for (let contador = 0; contador < listaDeTeclas.length; contador++) { 
-
-    const tecla =listaDeTeclas[contador]
-    const instrumento = tecla.classList[1]
-    const idAudio = #som_${instrumento} ; //template string 
-
-    tecla.onclick = function () {
-        tocaSom(idAudio);
-     }
-
-    tecla.onkeydown = function (evento) {
-       
-
-        if (evento.code ==='Space' || evento.code === 'Enter') {
-            tecla.classList.add('ativa');
-        
-        }
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
     }
 
-    tecla.onkeyup = function () {
-        tecla.classList.remove('ativa');
-    }
 }
 
+const contadores = document.querySelectorAll(".contandor");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2023-10-30T00:00:00");
+const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
+
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+
+
+function atualizaCronometro(){
+    for (let i=0; i<contadores.length;i++){
+        contadores[0].textContent = calculaTempo(tempos[i]);
+    }
+}
+atualizaCronometro();
+setInterval
+
+
+function calculaTempo(tempoObjetivo){
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(minutos/60);
+    let dias = Math.floor(horas/24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+    return dias + " dias " + horas + " horas " + minutos + "minutos" + segundos + " segundos"; 
+}
